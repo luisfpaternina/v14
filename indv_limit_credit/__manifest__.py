47d98e668097899adf64a0ec2d4d424dbe58d4aa
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-12 09:39:52 -0500

    add

--- indv_limit_credit/__manifest__.py
+++ indv_limit_credit/__manifest__.py
@@ -28,6 +28,7 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/credit_limit.xml',
+        'views/res_partner.xml',
                    
     ],
     'installable': True
