0087a1cc71ecee1d83b3973ba2b788541b798444
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-12 09:51:47 -0500

    Update res_partner.py

--- indv_limit_credit/models/res_partner.py
+++ indv_limit_credit/models/res_partner.py
@@ -8,4 +8,18 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     has_credit = fields.Boolean(
-        string="Has a current credit")
+        string="Has a current credit",
+        compute="compute_has_a_credit")
+    credit_value = fields.Float(
+        string="Credit value",
+        compute="compute_has_a_credit")
+
+
+    def compute_has_a_credit(self):
+        credit_obj = self.env['credit.limit'].search([('partner_id', '=', self.id), ('state', '=', 'approved')], limit=1)
+        if credit_obj:
+            self.has_credit = True
+            self.credit_value = credit_obj.credit_amount
+        else:
+            self.has_credit = False
+            self.credit_value = 0
