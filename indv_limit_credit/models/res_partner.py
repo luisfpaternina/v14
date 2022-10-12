73df49a19da2ed7e7ac2ceadbcc983e1a2d46cdf
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-12 09:54:17 -0500

    Update res_partner.py

--- indv_limit_credit/models/res_partner.py
+++ indv_limit_credit/models/res_partner.py
@@ -16,7 +16,9 @@ class ResPartner(models.Model):
 
 
     def compute_has_a_credit(self):
-        credit_obj = self.env['credit.limit'].search([('partner_id', '=', self.id), ('state', '=', 'approved')], limit=1)
+        credit_obj = self.env['credit.limit'].search([
+            ('partner_id', '=', self.id),
+            ('state', '=', 'approved')], limit=1)
         if credit_obj:
             self.has_credit = True
             self.credit_value = credit_obj.credit_amount
