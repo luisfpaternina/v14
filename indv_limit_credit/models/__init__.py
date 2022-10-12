30aab56eb413dbef2de7483eaf1f5c582bc9fb23
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-12 09:32:01 -0500

    add

--- indv_limit_credit/models/__init__.py
+++ indv_limit_credit/models/__init__.py
@@ -3,3 +3,4 @@
 from . import sale_order
 from . import account_move
 from . import credit_limit
+from . import res_partner
---
+++ indv_limit_credit/models/res_partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+import re
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    has_credit = fields.Boolean(
+        string="Has a current credit")
