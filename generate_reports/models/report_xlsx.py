71a286426bee24ddc588943c00553585fcfcf1d8
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-17 15:35:28 -0500

    add

--- generate_reports/models/report_xlsx.py
+++ generate_reports/models/report_xlsx.py
@@ -9,9 +9,6 @@ class ReportXlsx(models.AbstractModel):
 
 
     def generate_xlsx_report(self, workbook, data, lines):
-        # reporte XLSX
-        logging.info('*******************************************************************')
-        logging.info('Líneas', lines)
         format1 = workbook.add_format({'font_size': 14, 'align': 'vcenter', 'bold': True})
         format2 = workbook.add_format({'font_size': 10, 'align': 'vcenter'})
         sheet = workbook.add_worksheet('reporte lfpv')
