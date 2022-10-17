b5edb18e45dcd614d7abdfafc24c91338b394fb9
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-17 14:47:42 -0500

    Update report_xlsx.py

--- generate_reports/models/report_xlsx.py
+++ generate_reports/models/report_xlsx.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api, _
 
 
 class ReportXlsx(models.AbstractModel):
-    _name = 'report.generate_reports.report_xlsx'
+    _name = 'report.generate_reports.report_customer_xlsx'
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, lines):
