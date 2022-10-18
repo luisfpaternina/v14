96da7abd4cf479da01c65087f5a3f78b5858d35f
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-18 06:47:39 -0500

    add

--- introdoo_reports/models/report_xlsx.py
+++ introdoo_reports/models/report_xlsx.py
@@ -6,3 +6,10 @@ import logging
 class ReportXlsx(models.AbstractModel):
     _name = 'report.introdoo_reports.report_customer_xlsx'
     _inherit = 'report.report_xlsx.abstract'
+
+    def generate_xlsx_report(self, workbook, data, partners):
+        format1 = workbook.add_format({'font_size': 13, 'align': 'vcenter', 'bold': True})
+        format2 = workbook.add_format({'font_size': 10, 'align': 'vcenter'})
+        sheet = workbook.add_worksheet('Hoja canal Introdoo')
+        sheet.write(2, 2, 'Contactos', format1)
+        sheet.write(2, 3, partners.name, format2)
