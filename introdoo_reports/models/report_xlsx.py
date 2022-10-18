7ba5d9d82c835c6c0c45f4cc4cecf2f84b93a6dc
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-18 08:56:49 -0500

    Update report_xlsx.py

--- introdoo_reports/models/report_xlsx.py
+++ introdoo_reports/models/report_xlsx.py
@@ -7,9 +7,10 @@ class ReportXlsx(models.AbstractModel):
     _name = 'report.introdoo_reports.report_customer_xlsx'
     _inherit = 'report.report_xlsx.abstract'
 
-    def generate_xlsx_report(self, workbook, data, partners):
-        format1 = workbook.add_format({'font_size': 13, 'align': 'vcenter', 'bold': True})
-        format2 = workbook.add_format({'font_size': 10, 'align': 'vcenter'})
-        sheet = workbook.add_worksheet('Hoja canal Introdoo')
-        sheet.write(2, 2, 'Contactos', format1)
-        sheet.write(2, 3, partners.name, format2)
+    def generate_xlsx_report(self, workbook, data, sale):
+        for line in sale.order_line:
+            format1 = workbook.add_format({'font_size': 13, 'align': 'vcenter', 'bold': True})
+            format2 = workbook.add_format({'font_size': 10, 'align': 'vcenter'})
+            sheet = workbook.add_worksheet('Hoja canal Introdoo')
+            sheet.write(2, 2, 'Contactos', format1)
+            sheet.write(2, 3, sale.line.name, format2)
