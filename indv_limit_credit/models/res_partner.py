ff1136973c923c72cde5b3d4b77faafefc96e655
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-18 20:52:35 -0500

    Update res_partner.py

--- indv_limit_credit/models/res_partner.py
+++ indv_limit_credit/models/res_partner.py
@@ -31,6 +31,6 @@ class ResPartner(models.Model):
     def records_partners(self):
         exis_records = []
         for record in self:
-            if record.vat in exis_records:
+            if record.vat not in exis_records:
                 raise ValidationError('No se puede modificar el vat del contacto')
             exis_records.append(record.vat)
