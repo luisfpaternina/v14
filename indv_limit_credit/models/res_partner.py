eabdd6b8a0eb3a14980636ecb7d294f9d46ea6d0
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-10-18 21:23:23 -0500

    Update res_partner.py

--- indv_limit_credit/models/res_partner.py
+++ indv_limit_credit/models/res_partner.py
@@ -33,7 +33,7 @@ class ResPartner(models.Model):
                 record.has_credit = False
                 record.credit_value = 0
 
-    @api.depends('vat','name')
+    @api.depends('vat')
     def compute_new_vat(self):
         for record in self:
             if record.vat and record.validate_vat == False:
